db85f36e87f297de3279f174c4ebb218147a838d
mohammedaladhary <caladhary@gmail.com>
2023-07-29 22:59:10 +0400

    route planner

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using routePlanner;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        bool[][] mapMatrix = new bool[][]
+        {
+            new bool[] { false, false, true, true, true },
+            new bool[] { true, true, true, false, true },
+            new bool[] { false, true, false, true, true },
+            new bool[] { true, true, true, true, false },
+            new bool[] { true, false, true, true, true }
+        };
+
+        int fromRow = 0;
+        int fromColumn = 0;
+        int toRow = 4;
+        int toColumn = 4;
+
+        bool isRouteExists = RoutePlanner.RouteExists(mapMatrix, fromRow, fromColumn, toRow, toColumn);
+
+        if (isRouteExists)
+        {
+            Console.WriteLine("Destination is reachable!");
+        }
+        else
+        {
+            Console.WriteLine("Destination is not reachable.");
+        }
+    }
+}
---
+++ RoutePlanner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace routePlanner
+{
+	public class RoutePlanner
+        
+	{
+        public static bool RouteExists(bool[][] mapMatrix, int fromRow, int fromColumn, int toRow, int toColumn)
+        {
+            int numRows = mapMatrix.Length;
+            int numColumns = mapMatrix[0].Length;
+
+            // Helper function to perform DFS
+            bool DepthFirstSearch(int row, int col)
+            {
+                // Check if the current cell is out of bounds or not a road (false)
+                if (row < 0 || row >= numRows || col < 0 || col >= numColumns || !mapMatrix[row][col])
+                    return false;
+
+                // Check if the current cell is the destination
+                if (row == toRow && col == toColumn)
+                    return true;
+
+                // Mark the current cell as visited by setting it to false (to avoid revisiting)
+                mapMatrix[row][col] = false;
+
+                // Recursively explore adjacent cells
+                bool foundPath = DepthFirstSearch(row + 1, col) ||
+                                 DepthFirstSearch(row - 1, col) ||
+                                 DepthFirstSearch(row, col + 1) ||
+                                 DepthFirstSearch(row, col - 1);
+
+                return foundPath;
+            }
+
+            // Call DFS from the starting point (fromRow, fromColumn)
+            return DepthFirstSearch(fromRow, fromColumn);
+        }
+    }
+}
+
